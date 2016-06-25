7440f46929c54a84247bd6408fe9f63f47dd3217
Altaf Ali <altaf@fastmail.com>
2016-06-25 06:03:36 +0100

    removed deprecated files

--- pipelines/dataset_collector/dataset_collector.py
+++
@@ -1,9 +0,0 @@
-from tasks.pipeline import Pipeline
-import tasks.icews as icews
-
-class DatasetCollectorPipeline(Pipeline):
-    def requires(self):
-        return icews.DatasetCollector(pipeline=self)
-
-    def run(self):
-        pass
